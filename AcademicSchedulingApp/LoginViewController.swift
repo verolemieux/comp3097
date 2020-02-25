0f7b2719d114d4779c9f3614e31dfeed3d9492bc
Veronyque Lemieux <veronyque.lemieux@gmail.com>
2020-02-24 22:16:24 -0500

    Started Login Screen

---
+++ AcademicSchedulingApp/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  AcademicSchedulingApp
+//
+//  Copyright © 2020 Veronyque Lemieux. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var IBOUsername: UITextField!
+    @IBOutlet weak var IBOPassword: UITextField!
+    
+    @IBAction func IBOLogin(_ sender: Any) {
+        
+    }
+    
+    @IBAction func IBORegister(_ sender: Any) {
+        
+    }
+    
+    @IBAction func IBOForgotPassword(_ sender: Any) {
+        
+    }
+}
+
