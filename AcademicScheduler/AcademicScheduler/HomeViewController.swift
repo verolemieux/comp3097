c23e055b3766a1c9f591269423cae266a850da7f
Veronyque Lemieux <veronyque.lemieux@gmail.com>
2020-02-26 20:27:54 -0500

    created a few more screens

---
+++ AcademicScheduler/AcademicScheduler/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  AcademicScheduler
+//
+//  Copyright © 2020 Veronyque Lemieux. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+}
